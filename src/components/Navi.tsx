d505cad5f9f5b414fbebc7353a36b2ad821559a2
ayaosamaramadan <ayaramadan2011@gmail.com>
2025-02-28 04:28:24 +0200

    respons nav

--- src/components/Navi.tsx
+++ src/components/Navi.tsx
@@ -7,44 +7,48 @@ const Navi = () => {
   return (
     <>
       <div className="border-b-2 border-black-100">
-        <div className="bg-black flex p-4 justify-between pr-20">
-          <div className="pl-96">
-            <span className="text-white pr-3">
+        <div className="bg-black flex p-4 justify-between pr-4 2xl:pl-50 xl:pl-[15%] lg:pl-[5%] w-full">
+            <div className="pl-4 flex justify-center md:justify-start w-full md:w-auto sm:w-auto 2sm:w-[220px]">
+            <span className="text-white pr-3 text-sm xl:text-[15px] lg:text-[14px] md:text-[13px] sm:text-[13px] 2sm:text-[13px] ">
               Summer Sale For All Swim Suits And Free Express Delivery - OFF
               50%!
             </span>
-            <span className="font-bold text-white underline cursor-pointer">
+            <span className="font-bold text-white underline cursor-pointer xl:text-[15px] lg:text-[14px] md:text-[13px] sm:text-[13px] 2sm:text-[13px] 2xl:w-full xl:w-[80px] lg:w-[80px] md:w-[180px] 2m:w-[180px] 2sm:w-[310px]">
               Shop Now
             </span>
-          </div>
+            </div>
 
           <div className="text-white flex">
-            <span>English</span>
-            <i className="fa-solid fa-chevron-down pt-[4px] pl-3"></i>
+            <span className="xl:text-[15px] lg:text-[14px] md:text-[13px] sm:text-[13px] 2sm:text-[13px]">English</span>
+            <i className="fa-solid fa-chevron-down pt-1 pl-3 xl:text-[15px] lg:text-[14px] md:text-[13px] sm:text-[13px] 2sm:text-[13px]"></i>
           </div>
         </div>
 
-        <div className="flex mt-10 mb-5 justify-between">
-            <div className="text-2xl font-bold ml-10">Exclusive</div>
-          <ul className="flex">
-            <li className="mr-12 hover:underline">Home</li>
-            <li className="mr-12 hover:underline">Content</li>
-            <li className="mr-12 hover:underline">About</li>
-            <li className="mr-12 hover:underline">Sign Up</li>
+        <div className="flex flex-col lg:flex-row mt-10 mb-5 justify-between items-center">
+          <div className="2xl:text-2xl xl:text-2xl lg:text-[1.3rem] md:text-[1.6rem] sm:text-[1.6rem] 2sm:text-[1.6rem] font-bold ml-4 lg:ml-10 mb-4 lg:mb-0">
+            Exclusive
+          </div>
+          <ul className="flex flex-wrap justify-center xl:pl-3 2xl:text-[1rem] xl:text-[1rem] lg:text-[0.8rem] ">
+            <li className="2xl:mr-12 xl:mr-12 lg:mr-4 md:mr-6 sm:mr-5 2sm:mr-3 hover:underline">Home</li>
+            <li className="2xl:mr-12 xl:mr-12 lg:mr-4 md:mr-6 sm:mr-5 2sm:mr-3 hover:underline">Content</li>
+            <li className="2xl:mr-12 xl:mr-12 lg:mr-4 md:mr-6 sm:mr-5 2sm:mr-3 hover:underline">About</li>
+            <li className="2xl:mr-4 xl:mr-4 lg:mr-4 md:mr-6 sm:mr-5 2sm:mr-3 hover:underline">Sign Up</li>
           </ul>
 
-          <div className="flex bg-slate-100 p-2 px-4 rounded w-[300px] justify-between mt-[-10px]">
-            <input type="search" placeholder="What are you looking for?" className="bg-slate-100"/>
-            <CiSearch className="text-black text-[25px]" />
+          <div className="flex bg-slate-100 p-2 px-4 rounded w-full 2xl:lg:w-[300px] xl:w-[300px] md:w-[300px] sm:w-[290px] 2sm:w-[260px] lg:w-[200px] justify-between mt-4 lg:mt-0">
+            <input
+              type="search"
+              placeholder="What are you looking for?"
+              className="bg-slate-100 w-full"
+            />
+            <CiSearch className="text-black text-[25px] ml-2" />
           </div>
 
-          <div className="text-black flex text-[26px] mt-[-2px] mr-[50px]">
-             
-            <IoIosHeartEmpty className="mr-[20px]"/>
-            <IoCartOutline />
+          <div className="text-black flex 2xl:text-[26px] xl:text-[26px] sm:text-[22px] 2sm:text-[22px]  mt-4 lg:mt-0 lg:mr-[50px] xl:pl-5">
+            <IoIosHeartEmpty className="mr-[10px]" />
+            <IoCartOutline className="ml-[10px]" />
           </div>
         </div>
-
       </div>
     </>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,14 @@ export default {
   ],
   theme: {
     extend: {},
+    screens: {
+      '2sm': '320px',
+      'sm': '375px',
+      'md': '425px',
+      'lg': '768px',
+      'xl': '1024px',
+      '2xl': '1440px',
+    }
   },
   plugins: [],
 }
