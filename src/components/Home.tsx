b708701d4d07ee5a90384cf114a52bcc2b45921c
ayaosamaramadan <ayaramadan2011@gmail.com>
2025-02-28 05:58:45 +0200

    add countdown timer

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,12 +1,39 @@
+import { useEffect, useState } from "react";
 import { FaChevronRight } from "react-icons/fa";
 import { GoDotFill } from "react-icons/go";
 
 const Home = () => {
+
+  
+    const calcTime = () => {
+        let time={};
+        const now = +new Date("2025-12-31") - +new Date();
+   
+        time = {
+            days: 3,
+            hours: Math.floor((now / (1000 * 60 * 60)) % 24),
+            minutes: Math.floor((now / 1000 / 60) % 60),
+            seconds: Math.floor((now / 1000) % 60),
+          };
+
+          return time;
+        //   console.log(time);
+    };
+  const [time, setTime] = useState({});
+
+    useEffect(() => {
+        const timer = setInterval(() => {
+            setTime(calcTime());
+          }, 1000);
+      
+          return () => clearInterval(timer);
+   
+    }
+    , []);
+
   return (
     <>
       <div className="flex">
-
-
         <div className="border-r-2 border-black-100 w-[280px] pr-20">
           <div className="w-[250px] pt-10 pl-10">
             <ul>
@@ -27,7 +54,6 @@ const Home = () => {
           </div>
         </div>
 
-
         <div className="ml-20 mt-10">
           <div className="text-white bg-black w-[90%] h-[400px] p-10 ">
             <img
@@ -37,44 +63,56 @@ const Home = () => {
             />
             <div>
               <ul className="flex w-[10%] mt-10 ml-[40%]">
-                <li><GoDotFill /></li>
-                <li><GoDotFill /></li>
-                <li><GoDotFill /></li>
-                <li><GoDotFill /></li>
-                 <li><GoDotFill /></li>
+                <li>
+                  <GoDotFill />
+                </li>
+                <li>
+                  <GoDotFill />
+                </li>
+                <li>
+                  <GoDotFill />
+                </li>
+                <li>
+                  <GoDotFill />
+                </li>
+                <li>
+                  <GoDotFill />
+                </li>
               </ul>
             </div>
           </div>
         </div>
-
       </div>
 
       <div className="flex mt-10">
         <img src="/src/assets/Rectangle 17.png" alt="" />
         <p className="text-red-500 font-bold ml-5 mt-2">Today’s</p>
-       
       </div>
-      
+
       <div>
-        <div><h1>Flash Sales</h1>
         <div>
+          <h1>Flash Sales</h1>
+          <div>
             <ul className="flex">
-            <li className="pl-3">Days </li>
-            <li className="pl-3"><span>:</span> Hours</li>
-            <li className="pl-3"><span>:</span>Minutes</li>
-            <li className="pl-3"><span>:</span>Seconds</li>
+              <li className="pl-3">Days </li>
+              <li className="pl-3">
+                <span>:</span> Hours
+              </li>
+              <li className="pl-3">
+                <span>:</span>Minutes
+              </li>
+              <li className="pl-3">
+                <span>:</span>Seconds
+              </li>
             </ul>
             <ul className="flex">
-            <li className="p-5">00</li>
-            <li className="p-5">00</li>
-            <li className="p-5">00</li>
-            <li className="p-5">00</li>
+              <li className="p-5">{time.days || '00'}</li>
+              <li className="p-5">{time.hours || '00'}</li>
+              <li className="p-5">{time.minutes || '00'}</li>
+              <li className="p-5">{time.seconds || '00'}</li>
             </ul>
+          </div>
         </div>
-</div>
-
- 
-
       </div>
     </>
   );
