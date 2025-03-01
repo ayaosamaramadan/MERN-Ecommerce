304dfc3b45329478f96bd1ec4776ec9bdcc094a0
ayaosamaramadan <ayaramadan2011@gmail.com>
2025-03-01 02:01:57 +0200

    add footer

---
+++ src/components/Home/Header.tsx
@@ -0,0 +1,59 @@
+// import { useEffect, useState } from "react";
+import { FaChevronRight } from "react-icons/fa";
+import { GoDotFill } from "react-icons/go";
+
+
+const Header = () => {
+    return ( <> <div className="flex">
+        <div className="border-r-2 border-black-100 w-[280px] pr-20">
+          <div className="w-[250px] pt-10 pl-10">
+            <ul>
+              <li className="flex pb-4">
+                Woman’s Fashion <FaChevronRight className="mt-1 ml-[50px]" />
+              </li>
+              <li className="flex pb-4">
+                Men’s Fashion <FaChevronRight className="mt-1 ml-[79px]" />
+              </li>
+              <li className="pb-4">Electronics</li>
+              <li className="pb-4">Home & Lifestyle</li>
+              <li className="pb-4">Medicine</li>
+              <li className="pb-4">Sports & Outdoor</li>
+              <li className="pb-4">Baby’s & Toys</li>
+              <li className="pb-4">Groceries & Pets</li>
+              <li className="pb-4">Health & Beauty</li>
+            </ul>
+          </div>
+        </div>
+
+        <div className="ml-20 mt-10">
+          <div className="text-white bg-black w-[90%] h-[400px] p-10 ">
+            <img
+              src="https://res.cloudinary.com/dgjbaeyok/image/upload/v1740722034/iphoneSlider_yc1km0.jpg"
+              alt="image"
+              className="w-[100%]"
+            />
+            <div>
+              <ul className="flex w-[10%] mt-10 ml-[40%]">
+                <li>
+                  <GoDotFill />
+                </li>
+                <li>
+                  <GoDotFill />
+                </li>
+                <li>
+                  <GoDotFill />
+                </li>
+                <li>
+                  <GoDotFill />
+                </li>
+                <li>
+                  <GoDotFill />
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div></> );
+}
+ 
+export default Header;
--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,65 +1,16 @@
-// import { useEffect, useState } from "react";
-import { FaChevronRight } from "react-icons/fa";
-import { GoDotFill } from "react-icons/go";
+
 import Sales from "./Sales";
 import Besto from "./Besto";
+import Header from "./Header";
+import Footer from "./Footer";
 
 const Home = () => {
   return (
     <div className="mt-40">
-      <div className="flex">
-        <div className="border-r-2 border-black-100 w-[280px] pr-20">
-          <div className="w-[250px] pt-10 pl-10">
-            <ul>
-              <li className="flex pb-4">
-                Woman’s Fashion <FaChevronRight className="mt-1 ml-[50px]" />
-              </li>
-              <li className="flex pb-4">
-                Men’s Fashion <FaChevronRight className="mt-1 ml-[79px]" />
-              </li>
-              <li className="pb-4">Electronics</li>
-              <li className="pb-4">Home & Lifestyle</li>
-              <li className="pb-4">Medicine</li>
-              <li className="pb-4">Sports & Outdoor</li>
-              <li className="pb-4">Baby’s & Toys</li>
-              <li className="pb-4">Groceries & Pets</li>
-              <li className="pb-4">Health & Beauty</li>
-            </ul>
-          </div>
-        </div>
-
-        <div className="ml-20 mt-10">
-          <div className="text-white bg-black w-[90%] h-[400px] p-10 ">
-            <img
-              src="https://res.cloudinary.com/dgjbaeyok/image/upload/v1740722034/iphoneSlider_yc1km0.jpg"
-              alt="image"
-              className="w-[100%]"
-            />
-            <div>
-              <ul className="flex w-[10%] mt-10 ml-[40%]">
-                <li>
-                  <GoDotFill />
-                </li>
-                <li>
-                  <GoDotFill />
-                </li>
-                <li>
-                  <GoDotFill />
-                </li>
-                <li>
-                  <GoDotFill />
-                </li>
-                <li>
-                  <GoDotFill />
-                </li>
-              </ul>
-            </div>
-          </div>
-        </div>
-      </div>
-
+     <Header/>
       <Sales />
       <Besto />
+      <Footer/>
     </div>
   );
 };
