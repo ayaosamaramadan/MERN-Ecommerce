6856124c5060b45da33185bbdb4ee4c3a5c6bdc4
ayaosamaramadan <ayaramadan2011@gmail.com>
2025-03-15 01:04:09 +0200

    refactor: improve server setup by organizing middleware and routes

--- server/index.js
+++ server/index.js
@@ -2,18 +2,24 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const dotenv = require("dotenv");
+const bcrypt = require("bcryptjs");
 const userRoutes = require("./routes/userRoutes");
-require("dotenv").config();
+const User = require("./models/User");
+
+dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-
+// Middleware
 app.use(cors());
 app.use(bodyParser.json());
 
+// Routes
 app.use("/api/users", userRoutes);
 
+// MongoDB connection
 const uri = process.env.MONGODB_URI;
 
 mongoose.set("strictQuery", true);
@@ -31,13 +37,7 @@ mongoose
   });
 
 
-app.get("/", (req, res) => {
-  res.send({
-    actuveStatus: true,
-    error: false,
-  })
-});
-
+// Start the server
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
