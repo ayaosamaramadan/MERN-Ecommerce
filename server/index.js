2bf6651c259c213012b76e128278dbf43dd1018c
ayaosamaramadan <ayaramadan2011@gmail.com>
2025-03-15 01:17:21 +0200

    fix

--- server/index.js
+++ server/index.js
@@ -37,26 +37,7 @@ mongoose
   });
 
 
-app.put("/api/users/updateProfile", async (req, res) => {
-  const { userId, name, email, password } = req.body;
-  try {
-    const user = await User.findById(userId);
-    if (user) {
-      user.name = name || user.name;
-      user.email = email || user.email;
-      if (password) {
-        const salt = await bcrypt.genSalt(10);
-        user.password = await bcrypt.hash(password, salt); 
-      }
-      await user.save();
-      res.status(200).json({ message: "Profile updated successfully", user });
-    } else {
-      res.status(404).json({ message: "User not found" });
-    }
-  } catch (error) {
-    res.status(500).json({ message: "Server error" });
-  }
-});
+
 
 // Root route
 
