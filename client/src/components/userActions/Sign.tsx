9214ef5b98db1c2db69f3504ef6cb7036b39789b
ayaosamaramadan <ayaramadan2011@gmail.com>
2025-03-20 05:37:29 +0200

    fix: update API URL for user registration endpoint

--- client/src/components/userActions/Sign.tsx
+++ client/src/components/userActions/Sign.tsx
@@ -88,7 +88,7 @@ const Sign = () => {
     if (!validate()) return;
 
     const apiUrl =
-      "http://localhost:5000/api/users/register";
+      "https://ecommerce-serv.vercel.app/api/users/register";
     try {
       const response = await fetch(apiUrl, {
         method: "POST",
